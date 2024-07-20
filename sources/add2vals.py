fee4a7be817521f3e2396e18a24aee8245808b78
Muhammad Fiqih A <muhammadfiqih910@gmail.com>
2024-07-20 22:57:52 +0700

    modify add2vals

--- sources/add2vals.py
+++ sources/add2vals.py
@@ -1,46 +1,46 @@
-# '''
-# A simple command line tool that takes 2 values and adds them together using
-# the calc.py library's 'add2' function.
-# '''
+'''
+A simple command line tool that takes 2 values and adds them together using
+the calc.py library's 'add2' function.
+'''
 
-# import sys
-# import calc
-
-# argnumbers = len(sys.argv) - 1
-
-# if argnumbers == 2 :
-#     print("")
-#     print("The result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]))))
-#     print("")
-#     sys.exit(0)
-
-# if argnumbers != 2 :
-#     print("")
-#     print("You entered " + str(argnumbers) + " value/s.")
-#     print("")
-#     print("Usage: 'add2vals X Y' where X and Y are individual values.")
-#     print("       If add2vals is not in your path, usage is './add2vals X Y'.")
-#     print("       If unbundled, usage is 'python add2vals.py X Y'.")
-#     print("")
-#     sys.exit(1)
-
-from flask import Flask, request, jsonify
+import sys
 import calc
 
-app = Flask(__name__)
+argnumbers = len(sys.argv) - 1
 
-@app.route('/add2vals', methods=['POST'])
-def add2vals():
-    data = request.json
-    x = data.get('x')
-    y = data.get('y')
-    if x is None or y is None:
-        return jsonify({'error': 'Missing parameter'}), 400
-    try:
-        result = float(x) + float(y)
-        return jsonify({'result': result})
-    except ValueError:
-        return jsonify({'error': 'Invalid parameter'}), 400
+if argnumbers == 2 :
+    print("")
+    print("The result is " + str(calc.add2(str(sys.argv[1]), str(sys.argv[2]))))
+    print("")
+    sys.exit(0)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+if argnumbers != 2 :
+    print("")
+    print("You entered " + str(argnumbers) + " value/s.")
+    print("")
+    print("Usage: 'add2vals X Y' where X and Y are individual values.")
+    print("       If add2vals is not in your path, usage is './add2vals X Y'.")
+    print("       If unbundled, usage is 'python add2vals.py X Y'.")
+    print("")
+    sys.exit(1)
+
+# from flask import Flask, request, jsonify
+# import calc
+
+# app = Flask(__name__)
+
+# @app.route('/add2vals', methods=['POST'])
+# def add2vals():
+#     data = request.json
+#     x = data.get('x')
+#     y = data.get('y')
+#     if x is None or y is None:
+#         return jsonify({'error': 'Missing parameter'}), 400
+#     try:
+#         result = float(x) + float(y)
+#         return jsonify({'result': result})
+#     except ValueError:
+#         return jsonify({'error': 'Invalid parameter'}), 400
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=3000)
